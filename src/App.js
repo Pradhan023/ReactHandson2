9f312f27f93b2791617064e9a02dbecd815c0f46
Anish Pradhan <anishpradha5523@gmail.com>
2023-05-29 14:40:07 +0530

    React-Handson3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+export default class handsonform extends React.Component {
+
+    state={
+        name:"",
+        department:"",
+        rating:"",
+        data:[]
+    }
+
+    change=(event)=>{
+      this.setState({[event.target.name]:event.target.value})
+    }
+
+    display=()=>{
+        const obj = {
+            name : this.state.name,
+            department : this.state.department,
+            rating : this.state.rating
+        }
+        this.state.data.push(obj)
+        this.setState({data:this.state.data,name:"",department:"",rating:""})
+    }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <h1>Employee Feedback Form</h1>
+        </div>
+
+        <div>
+        <label for= "name">Name </label>
+        <input type="text" id="name" name='name' onChange={this.change} placeholder="Enter Your Name" value={this.state.name}>
+        </input>
+        </div>
+  
+        <div>
+        <label for= "dep">Department </label>
+        <input type="text" name='department' placeholder='Enter Your Department Name' onChange={this.change} id="dep" value={this.state.department}>
+        </input>
+        </div>
+  
+        <div>
+        <label for= "rate">Rating </label>
+        <input type="number" name='rating' placeholder='Rating' onChange={this.change} id="rate" min="0" max="10" value={this.state.rating}>
+        </input>
+        </div>
+  
+        <div>
+        <button onClick={this.display}>Submit</button>
+        </div>
+
+        <div className='data-container'>
+        {this.state.data.map((item)=>{
+          return(
+            <h3>{`Name: ${item.name} | Department:${item.department} | Rating: ${item.rating}`}</h3>
+          )
+        })}
+        </div>
+  
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+
+
